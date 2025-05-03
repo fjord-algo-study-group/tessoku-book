1b626fe452bb5eb14e632afccb815ed9e8ab9482
yuma <ikeoku.y@gmail.com>
2025-05-03 11:18:02 +0900

    人生の優先度はお金か？それとも、、、

---
+++ chapter8/b39.rb
@@ -0,0 +1,109 @@
+class PriorityQueue
+  def initialize(array = [], &comp)
+    @heap = array
+    @comp = comp || proc { |x, y| x > y }
+    heapify
+  end
+
+  def self.[](*array, &comp)
+    new(array, &comp)
+  end
+
+  attr_reader :heap
+  alias to_a heap
+
+  # Push new element to the heap.
+  def push(item)
+    shift_down(0, @heap.push(item).size - 1)
+    self
+  end
+
+  alias << push
+  alias append push
+
+  # Pop the element with the highest priority.
+  def pop
+    latest = @heap.pop
+    return latest if empty?
+
+    ret_item = heap[0]
+    heap[0] = latest
+    shift_up(0)
+    ret_item
+  end
+
+  alias shift pop
+
+  # Get the element with the highest priority.
+  def get
+    @heap[0]
+  end
+
+  alias top get
+  alias first get
+
+  # Returns true if the heap is empty.
+  def empty?
+    @heap.empty?
+  end
+
+  def size
+    @heap.size
+  end
+
+  def to_s
+    "<#{self.class}: @heap:(#{heap.join(', ')}), @comp:<#{@comp.class} #{@comp.source_location.join(':')}>>"
+  end
+
+  private
+
+  def heapify
+    (@heap.size / 2 - 1).downto(0) { |i| shift_up(i) }
+  end
+
+  def shift_up(pos)
+    end_pos = @heap.size
+    start_pos = pos
+    new_item = @heap[pos]
+    left_child_pos = 2 * pos + 1
+
+    while left_child_pos < end_pos
+      right_child_pos = left_child_pos + 1
+      if right_child_pos < end_pos && @comp.call(@heap[right_child_pos], @heap[left_child_pos])
+        left_child_pos = right_child_pos
+      end
+      # Move the higher priority child up.
+      @heap[pos] = @heap[left_child_pos]
+      pos = left_child_pos
+      left_child_pos = 2 * pos + 1
+    end
+    @heap[pos] = new_item
+    shift_down(start_pos, pos)
+  end
+
+  def shift_down(star_pos, pos)
+    new_item = @heap[pos]
+    while pos > star_pos
+      parent_pos = (pos - 1) >> 1
+      parent = @heap[parent_pos]
+      break if @comp.call(parent, new_item)
+
+      @heap[pos] = parent
+      pos = parent_pos
+    end
+    @heap[pos] = new_item
+  end
+end
+
+pq = PriorityQueue.new
+
+n, d = gets.split.map(&:to_i)
+xy = Array.new(n) { gets.split.map(&:to_i) }
+
+jobs = Hash.new { |h, k| h[k] = [] }
+xy.each { |x, y| jobs[x] << y }
+
+puts (1..d).sum { |day|
+  jobs[day].each { pq << _1 }
+  pq.shift || 0
+}
